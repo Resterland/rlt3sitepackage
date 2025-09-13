05bc774632f992d11ee53507b64a2d9a84d776b1
Roland Fuhrer <roland@resterland.ch>
2025-09-13 16:56:56 +0200

    adding editorconfig and gitignore

--- Resources/Public/JavaScript/rlmenu.js
+++ Resources/Public/JavaScript/rlmenu.js
@@ -1,12 +1,3 @@
 ;(function ($) {
-    $(function () {
-        $('nav ul li > a:not(:only-child)').click(function (e) {
-            $(this).siblings('.nav-dropdown').toggle()
-            $('.nav-dropdown').not($(this).siblings()).hide()
-            e.stopPropagation()
-        })
-        $('html').click(function () {
-            $('.nav-dropdown').hide()
-        })
-    })
+    // Begin jQuery
 })(jQuery)
