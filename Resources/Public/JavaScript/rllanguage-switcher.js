5be250fe5798e25b58c01d0903dcd7767c790479
Roland Fuhrer <roland@resterland.ch>
2026-02-01 12:10:13 +0100

    preparing release v2.1.0

---
+++ Resources/Public/JavaScript/rllanguage-switcher.js
@@ -0,0 +1,9 @@
+import { LitElement, html } from "lit";
+
+import {randomIdString} from "utils";
+
+export class RlLanguageSwitcher extends LitElement {
+}
+
+
+customElements.define("rl-language-switcher", RlLanguageSwitcher);
