ffe94ba3900fcf53bac580a92ec2178a2f9cf04c
Roland Fuhrer <roland@resterland.ch>
2026-02-01 19:50:42 +0100

    preparing release v2.1.0

--- Resources/Public/JavaScript/color-scheme-switch.js
+++ Resources/Public/JavaScript/color-scheme-switch.js
@@ -3,14 +3,51 @@ import {classMap} from 'lit/directives/class-map.js';
 
 class RLColorTheme extends LitElement {
   static properties = {
-    list: {},
-    condition: {},
+    _mode: { state: true },
   };
 
   constructor() {
     super();
-    this.list = ['light dark', 'light', 'dark'];
-    this.condition = true;
+    this._mode = "light dark";
+    this._options = Object.entries({
+      "light dark": this._mode("theme-default")`OS default`,
+      light: this._mode`Light`,
+      dark: this._mode`Dark`,
+    });
+  }
+
+  /** @param {MouseEvent} event */
+  _setMode({ target }) {
+    if (target instanceof HTMLElement) {
+      const mode = target.dataset.mode;
+      if (mode === "light dark" || mode === "light" || mode === "dark") {
+        this._mode = mode;
+        try {
+          localStorage.setItem("theme", mode);
+        } catch (error) {
+          console.warn("Unable to write theme to localStorage", error);
+        }
+        const dropdown = this.shadowRoot?.querySelector("rl-dropdown");
+        if (dropdown) {
+          dropdown.open = false;
+        }
+      }
+    }
+  }
+  /**
+   * @param {import("lit").PropertyValues<this>} changedProperties
+   */
+  willUpdate(changedProperties) {
+    if (changedProperties.has("_mode") && globalThis.document) {
+      document.documentElement.dataset.theme = this._mode;
+      this.dispatchEvent(
+        new CustomEvent("rl-color-theme-update", {
+          bubbles: true,
+          composed: true,
+          detail: this._mode,
+        }),
+      );
+    }
   }
 
   render() {
@@ -21,22 +58,33 @@ class RLColorTheme extends LitElement {
             part="button"
             slot="button"
             class="color-theme__button"
+            data-mode=${this._mode}
             type="button"
+            aria-label=${this._mode`Switch color theme`}
           >
-            <span>Themes</span>
+            <span>${this._mode`Theme`}</span>
           </button>
           <div
             slot="dropdown"
             class="color-theme__dropdown"
             id="color-theme__dropdown"
           >
-      <ul class="color-theme__list">
-        ${this.list.map(
-      (item, index) => html`
-              <li>${index}: ${item}</li>
-            `
-    )}
-      </ul>
+            <ul class="color-theme__list">
+              ${this._options.map(
+                ([mode, option]) =>
+                  html`<li>
+                    <button
+                      class="color-theme__option"
+                      data-mode=${mode}
+                      ?data-current=${mode === this._mode}
+                      type="button"
+                      @click=${this._setMode}
+                    >
+                      ${option}
+                    </button>
+                  </li>`,
+              )}
+            </ul>
           </div>
         </rl-dropdown>
       </div>
