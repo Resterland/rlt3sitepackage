afc76f349d1cf098a597d11eee0eae600a506d35
Roland Fuhrer <roland@resterland.ch>
2026-02-01 17:14:09 +0100

    preparing release v2.1.0

--- Resources/Public/JavaScript/color-scheme-switch.js
+++ Resources/Public/JavaScript/color-scheme-switch.js
@@ -2,7 +2,7 @@ import { LitElement, html, css } from "lit";
 
 import {RLDropdown} from "rldropdown";
 
-export class RLColorTheme extends RLDropdown(LitElement) {
+export class RLColorTheme extends LitElement {
 }
 
 customElements.define("rl-color-theme", RLColorTheme);
