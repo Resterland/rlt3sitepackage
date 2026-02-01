d45ef320ff0bfdbac6bb93694f965c0f6ed0c6e9
Roland Fuhrer <roland@resterland.ch>
2026-02-01 18:41:17 +0100

    preparing release v2.1.0

--- Resources/Public/JavaScript/color-scheme-switch.js
+++ Resources/Public/JavaScript/color-scheme-switch.js
@@ -2,7 +2,42 @@ import { LitElement, html, css } from "lit";
 
 class RLColorTheme extends LitElement {
   render() {
-    return html`<p>Hello from my template.</p>`;
+    return html`<div class="color-theme">
+      <rl-dropdown>
+        <button
+          part="button"
+          slot="button"
+          class="color-theme__button"
+          data-mode=${this._mode}
+          type="button"
+          aria-label=${this.l10n`Switch color theme`}
+        >
+          <span>${this.l10n`Theme`}</span>
+        </button>
+        <div
+          slot="dropdown"
+          class="color-theme__dropdown"
+          id="color-theme__dropdown"
+        >
+          <ul class="color-theme__list">
+            ${this._options.map(
+              ([mode, option]) =>
+                html`<li>
+                  <button
+                    class="color-theme__option"
+                    data-mode=${mode}
+                    ?data-current=${mode === this._mode}
+                    type="button"
+                    @click=${this._setMode}
+                  >
+                    ${option}
+                  </button>
+                </li>`,
+            )}
+          </ul>
+        </div>
+      </rl-dropdown>
+    </div>`;
   }
 
 }
