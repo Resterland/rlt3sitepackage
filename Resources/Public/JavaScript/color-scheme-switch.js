814e0cd536df0051522fc4c692cc208ea43304b9
Roland Fuhrer <roland@resterland.ch>
2026-02-06 12:24:39 +0100

    preparing release v2.1.0

--- Resources/Public/JavaScript/color-scheme-switch.js
+++ Resources/Public/JavaScript/color-scheme-switch.js
@@ -2,16 +2,16 @@ const html = document.querySelector('html');
 
 // three functions used by buttons and radios
 function switchAuto() {
-  html.style.setProperty("data-theme", "light dark");
+  html.style.setProperty("color-scheme", "light dark");
 }
 function switchLight() {
-  html.style.setProperty("data-theme", "light");
+  html.style.setProperty("color-scheme", "light");
 }
 function switchDark() {
-  html.style.setProperty("data-theme", "dark");
+  html.style.setProperty("color-scheme", "dark");
 }
 
 // single function used by select element
 function switchMode(mode) {
-  html.style.setProperty("data-theme", mode === "auto" ? "light dark" : mode);
+  html.style.setProperty("color-scheme", mode === "auto" ? "light dark" : mode);
 }
