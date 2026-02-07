17b1f4c3ae35488e82838dcba5d1fc0158407c49
Roland Fuhrer <roland@resterland.ch>
2026-02-07 15:59:43 +0100

    preparing release v2.1.0

--- Resources/Public/JavaScript/color-scheme.js
+++ Resources/Public/JavaScript/color-scheme.js
@@ -1,52 +1,44 @@
-'use strict';'use strict';
-(function () {
-  function init() {
-    const html = document.querySelector("html");
-    const mode = localStorage.getItem("mode");
-    const autoSwitch = document.getElementById('autoSwitch');
-    autoSwitch.addEventListener('onclick', switchAuto);
-    const lightSwitch = document.getElementById('lightSwitch');
-    lightSwitch.addEventListener('onclick', switchLight);
-    const darkSwitch = document.getElementById('darkSwitch');
-    lightSwitch.addEventListener('onclick', switchDark);
-  }
+'use strict';
+const html = document.querySelector("html");
+const mode = localStorage.getItem("mode");
+const autoSwitch = document.querySelector('#autoSwitch');
+const lightSwitch = document.querySelector('#lightSwitch');
+const darkSwitch = document.querySelector('#darkSwitch');
+document.querySelector('#autoSwitch').addEventListener("click", () => {
+  switchAuto(CSS.escape("this?element"));
+});
+document.querySelector('#lightSwitch').addEventListener("click", () => {
+  switchLight(CSS.escape("this?element"));
+});
+document.querySelector('#darkSwitch').addEventListener("click", () => {
+  switchDark(CSS.escape("this?element"));
+});
 
-  function switchLight() {
-    html.style.setProperty("color-scheme", "light");
-    localStorage.setItem("mode", "light");
-    lightSwitch.setAttribute("aria-pressed","true");
-    darkSwitch.setAttribute("aria-pressed","false");
-    autoSwitch.setAttribute("aria-pressed","false");
-  }
+if (mode === "light") switchLight();
+if (mode === "dark") switchDark();
 
-  function switchDark() {
-    html.style.setProperty("color-scheme", "dark");
-    localStorage.setItem("mode", "dark");
-    lightSwitch.setAttribute("aria-pressed","false");
-    darkSwitch.setAttribute("aria-pressed","true");
-    autoSwitch.setAttribute("aria-pressed","false");
-  }
+function switchAuto() {
+  html.style.setProperty("color-scheme", "light dark");
+  localStorage.removeItem("mode");
+  lightSwitch.setAttribute("aria-pressed","false");
+  darkSwitch.setAttribute("aria-pressed","false");
+  autoSwitch.setAttribute("aria-pressed","true");
+}
 
-  function switchAuto() {
-    html.style.setProperty("color-scheme", "light dark");
-    localStorage.removeItem("mode");
-    lightSwitch.setAttribute("aria-pressed","false");
-    darkSwitch.setAttribute("aria-pressed","false");
-    autoSwitch.setAttribute("aria-pressed","true");
-  }
+function switchLight() {
+  html.style.setProperty("color-scheme", "light");
+  localStorage.setItem("mode", "light");
+  lightSwitch.setAttribute("aria-pressed","true");
+  darkSwitch.setAttribute("aria-pressed","false");
+  autoSwitch.setAttribute("aria-pressed","false");
+}
 
-  function erzeugeZeitStempel() {
-    const zeitstempeltext = document.createTextNode(document.lastModified),
-      textZuvor = document.createTextNode('Datum des letzten Updates: ');
-    document.getElementById('absatz')
-      .appendChild(textZuvor);
-    document.getElementById('absatz')
-      .appendChild(zeitstempeltext);
-  }
-  let mode;
-  if (mode === "light") switchLight();
-  if (mode === "dark") switchDark();
-  document.addEventListener('DOMContentLoaded', init);
-}());
+function switchDark() {
+  html.style.setProperty("color-scheme", "dark");
+  localStorage.setItem("mode", "dark");
+  lightSwitch.setAttribute("aria-pressed","false");
+  darkSwitch.setAttribute("aria-pressed","true");
+  autoSwitch.setAttribute("aria-pressed","false");
+}
 
 // there are likely DRYer ways to do this...
