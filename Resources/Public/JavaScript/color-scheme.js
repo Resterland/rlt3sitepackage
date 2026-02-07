1f578c3ee644f9e9732802dc79d4f310e931b668
Roland Fuhrer <roland@resterland.ch>
2026-02-07 13:45:19 +0100

    preparing release v2.1.0

--- Resources/Public/JavaScript/color-scheme.js
+++ Resources/Public/JavaScript/color-scheme.js
@@ -1,84 +1,16 @@
-import {LitElement, html} from 'lit';
+'use strict';
+const body = document.querySelector('body');
+const hueSlider = document.querySelector('#hueSlider');
 
-import "rldropdown";
-
-export class ColorSchemeSwitch extends LitElement {
-
-
-  static properties = {
-    _mode: { state: true },
-    _options: {},
-    themes: {},
-  };
-
-  constructor() {
-    super();
-    this._mode = "light dark";
-    this._options = Object.entries({
-      "light dark": this.themes("theme-default")`OS default`,
-      light: this.themes`Light`,
-      dark: this.themes`Dark`,
-    });
-  }
-
-  /** @param {MouseEvent} event */
-  _setMode({ target }) {
-    if (target instanceof HTMLElement) {
-      const mode = target.dataset.mode;
-      if (mode === "light dark" || mode === "light" || mode === "dark") {
-        this._mode = mode;
-        try {
-          localStorage.setItem("theme", mode);
-        } catch (error) {
-          console.warn("Unable to write theme to localStorage", error);
-        }
-        const dropdown = this.shadowRoot?.querySelector("rl-dropdown");
-        if (dropdown) {
-          dropdown.open = false;
-        }
-      }
-    }
-  }
-
-  render() {
-    return html`
-      <div class="color-theme">
-        <rl-dropdown>
-          <button
-            part="button"
-            slot="button"
-            class="color-theme__button"
-            data-mode=${this._mode}
-            type="button"
-            aria-label=${this.themes`Switch color theme`}
-          >
-            <span>${this.themes`Theme`}</span>
-          </button>
-          <div
-            slot="dropdown"
-            class="color-theme__dropdown"
-            id="color-theme__dropdown"
-          >
-            <ul class="color-theme__list">
-              ${this._options.map(
-                ([mode, option]) =>
-                  html`<li>
-                  <button
-                    class="color-theme__option"
-                    data-mode=${mode}
-                    ?data-current=${mode === this._mode}
-                    type="button"
-                    @click=${this._setMode}
-                  >
-                    ${option}
-                  </button>
-                </li>`,
-              )}
-            </ul>
-          </div>
-        </rl-dropdown>
-      </div>
-  `;
-  }
+function switchDark() {
+  body.style.setProperty("color-scheme", "dark");
 }
-customElements.define('color-scheme-switch', ColorSchemeSwitch);
+function switchLight() {
+  body.style.setProperty("color-scheme", "light");
+}
+function switchAuto() {
+  body.style.setProperty("color-scheme", "light dark");
+}
+
+hueSlider.addEventListener("input", () =>
+  body.style.setProperty("--hue", hueSlider.value));
