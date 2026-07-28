61305ff3bc50c89ee9c9d4496678be6d93057c28
Resterland <roland@resterland.ch>
2026-07-28 10:16:47 +0200

    adding placeholder plugin to ckeditor

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,6 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') or die();
 
-#$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:rlt3sitepackage/Configuration/RTE/Custom.yaml';
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default']
-    = 'EXT:rlt3sitepackage/Configuration/RTE/RlDefault.yaml';
+    = 'EXT:rlt3sitepackage/Configuration/RTE/RlCustom.yaml';
 
