35478062472187083cd699b659a49a67dc809e0f
Resterland <roland@resterland.ch>
2026-07-31 10:59:14 +0200

    adding placeholder plugin to ckeditor

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -14,10 +14,11 @@
  */
 
 return [
-    'dependencies' => ['core'],
+    'dependencies' => ['core', 'backend'],
     'imports' => [
         'lit' => 'EXT:core/Resources/Public/JavaScript/Contrib/lit/index.js',
         'utils' => 'EXT:rlt3sitepackage/Resources/Public/JavaScript/utils/index.js',
         'rldropdown' => 'EXT:rlt3sitepackage/Resources/Public/JavaScript/rldropdown.js',
+        '@resterland/rlt3sitepackage/placeholder.js' => 'EXT:rlt3sitepackage/Resources/Public/JavaScript/ckeditor/placeholder.js',
     ],
 ];
