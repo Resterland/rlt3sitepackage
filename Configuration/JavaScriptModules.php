7434357683362df36b15f6382e68ab86f018ab22
Resterland <roland@resterland.ch>
2026-09-13 10:56:17 +0200

    update site package

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -14,11 +14,10 @@
  */
 
 return [
-    'dependencies' => ['core', 'backend'],
+    'dependencies' => ['core'],
     'imports' => [
         'lit' => 'EXT:core/Resources/Public/JavaScript/Contrib/lit/index.js',
         'utils' => 'EXT:rlt3sitepackage/Resources/Public/JavaScript/utils/index.js',
         'rldropdown' => 'EXT:rlt3sitepackage/Resources/Public/JavaScript/rldropdown.js',
-        'placeholder' => 'EXT:rlt3sitepackage/Resources/Public/JavaScript/ckeditor/placeholder.js',
     ],
 ];
