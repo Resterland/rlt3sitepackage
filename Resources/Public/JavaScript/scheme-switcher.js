509c40306f4b0fa95963804c86c23d619152ae3b
Resterland <roland@resterland.ch>
2026-09-13 16:26:11 +0200

    update site package

--- Resources/Public/JavaScript/scheme-switcher.js
+++ Resources/Public/JavaScript/scheme-switcher.js
@@ -1,24 +1,18 @@
 'use strict';
 
-import { LitElement as u,html as s,nothing as m} from"lit";
-import { property as d,state as p,customElement as b} from"lit/decorators.js";
-import v from"@typo3/core/ajax/ajax-request.js";
-import"@typo3/backend/element/icon-element.js";
-
 const html = document.querySelector('html');
 
-// three functions used by buttons
 function switchAuto() {
-  html.style.setProperty("base-scheme", "light dark");
+  html.style.setProperty("color-scheme", "light dark");
 }
 function switchLight() {
-  html.style.setProperty("base-scheme", "light");
+  html.style.setProperty("color-scheme", "light");
 }
 function switchDark() {
-  html.style.setProperty("base-scheme", "dark");
+  html.style.setProperty("color-scheme", "dark");
 }
 
 // single function used by select element and radio fieldset
 function switchMode(mode) {
-  html.style.setProperty("base-scheme", mode === "auto" ? "light dark" : mode);
+  html.style.setProperty("color-scheme", mode === "auto" ? "light dark" : mode);
 }
