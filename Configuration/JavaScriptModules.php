df171a45a63193ade1778d56ca7adcbc733a978b
Resterland <roland@resterland.ch>
2026-09-18 10:36:58 +0200

    update site package

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -19,7 +19,5 @@ return [
         'lit' => 'EXT:core/Resources/Public/JavaScript/Contrib/lit/index.js',
         'utils' => 'EXT:rlt3sitepackage/Resources/Public/JavaScript/utils/index.js',
         'rldropdown' => 'EXT:rlt3sitepackage/Resources/Public/JavaScript/rldropdown.js',
-        'scheme-switcher' => 'EXT:rlt3sitepackage/Resources/Public/JavaScript/scheme-switcher.js',
-        'hook' => 'EXT:rlt3sitepackage/Resources/Public/JavaScript/hook.js',
     ],
 ];
