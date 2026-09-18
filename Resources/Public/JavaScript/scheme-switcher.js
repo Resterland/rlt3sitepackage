cc2984480ae7d01428f9c149a1d5c05e4db120be
Resterland <roland@resterland.ch>
2026-09-18 10:19:52 +0200

    update site package

--- Resources/Public/JavaScript/scheme-switcher.js
+++ Resources/Public/JavaScript/scheme-switcher.js
@@ -2,6 +2,18 @@
 
 const html = document.querySelector('html');
 const mode = localStorage.getItem("mode");
+const autoSwitch = document.querySelector('#autoSwitch');
+const lightSwitch = document.querySelector('#lightSwitch');
+const darkSwitch = document.querySelector('#darkSwitch');
+document.querySelector('#autoSwitch').addEventListener("click", () => {
+  switchAuto(CSS.escape("this?element"));
+});
+document.querySelector('#lightSwitch').addEventListener("click", () => {
+  switchLight(CSS.escape("this?element"));
+});
+document.querySelector('#darkSwitch').addEventListener("click", () => {
+  switchDark(CSS.escape("this?element"));
+});
 
 if (mode === "light") switchLight();
 if (mode === "dark") switchDark();
@@ -9,14 +21,32 @@ if (mode === "dark") switchDark();
 function switchAuto() {
   html.style.setProperty("color-scheme", "light dark");
   localStorage.removeItem("mode");
+  lightSwitch.setAttribute("aria-pressed","false");
+  darkSwitch.setAttribute("aria-pressed","false");
+  autoSwitch.setAttribute("aria-pressed","true");
+  lightSwitch.setAttribute("data-mode","light");
+  darkSwitch.setAttribute("data-mode","dark");
+  autoSwitch.setAttribute("data-mode","light dark");
 }
 
 function switchLight() {
   html.style.setProperty("color-scheme", "light");
   localStorage.setItem("mode", "light");
+  lightSwitch.setAttribute("aria-pressed","true");
+  darkSwitch.setAttribute("aria-pressed","false");
+  autoSwitch.setAttribute("aria-pressed","false");
+  lightSwitch.setAttribute("data-mode","light");
+  darkSwitch.setAttribute("data-mode","dark");
+  autoSwitch.setAttribute("data-mode","light dark");
 }
 
 function switchDark() {
   html.style.setProperty("color-scheme", "dark");
   localStorage.setItem("mode", "dark");
+  lightSwitch.setAttribute("aria-pressed","false");
+  darkSwitch.setAttribute("aria-pressed","true");
+  autoSwitch.setAttribute("aria-pressed","false");
+  lightSwitch.setAttribute("data-mode","light");
+  darkSwitch.setAttribute("data-mode","dark");
+  autoSwitch.setAttribute("data-mode","light dark");
 }
