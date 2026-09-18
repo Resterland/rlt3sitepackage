cde3436a8a61cb5f3bab725047fec1f35dac5482
Resterland <roland@resterland.ch>
2026-09-18 11:02:18 +0200

    update site package

--- Resources/Public/JavaScript/light-dark-switch.js
+++ Resources/Public/JavaScript/light-dark-switch.js
@@ -5,6 +5,16 @@ const autoSwitch = document.querySelector('#autoSwitch');
 const lightSwitch = document.querySelector('#lightSwitch');
 const darkSwitch = document.querySelector('#darkSwitch');
 
+document.querySelector('#autoSwitch').addEventListener("click", () => {
+  switchAuto(CSS.escape("this?element"));
+});
+document.querySelector('#lightSwitch').addEventListener("click", () => {
+  switchLight(CSS.escape("this?element"));
+});
+document.querySelector('#darkSwitch').addEventListener("click", () => {
+  switchDark(CSS.escape("this?element"));
+});
+
 if (mode === "light") switchLight();
 if (mode === "dark") switchDark();
 
